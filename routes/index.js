a5536575d8a447335ce5ff7f2a892cf540c19e1f
alfredojm <alfredojmangado@gmail.com>
2015-07-21 01:08:09 +0200

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var MiQuizController =require('../controllers/MiQuiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'MiQuiz' });
 });
+router.get('/quizes/question', quizController.question);  
+router.get('/quizes/answer',   quizController.answer); 
 
 module.exports = router;
