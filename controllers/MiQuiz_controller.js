b171b83a76a3fe42ed3c0ba9a497b377ce0a5814
alfredojm <alfredojmangado@gmail.com>
2015-07-22 22:36:39 +0200

    Despliegue en Heroku

---
+++ controllers/MiQuiz_controller.js
@@ -0,0 +1,14 @@
+// GET /quizes/question 
+exports.question = function(req, res) { 
+    res.render('quizes/question', {pregunta: 'Capital de Navarra'}); 
+ }; 
+ 
+ 
+// GET /quizes/answer 
+ exports.answer = function(req, res) { 
+    if (req.query.respuesta === 'Pamplona'){ 
+       res.render('quizes/answer', {respuesta: 'Correcto'}); 
+    } else { 
+       res.render('quizes/answer', {respuesta: 'Incorrecto'}); 
+    } 
+ }; 
